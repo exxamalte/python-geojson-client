bda6b4f12873cbabca49021809073969dc505ede
Malte Franken <exxamalte@users.noreply.github.com>
2022-02-25 21:15:24 +1100

    Setup cleanup (#25)
    * initial clean-up
    
    * move version number

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+import os
+
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+NAME = "geojson_client"
+AUTHOR = "Malte Franken"
+AUTHOR_EMAIL = "coding@subspace.de"
+DESCRIPTION = "A GeoJSON client library."
+URL = "https://github.com/exxamalte/python-geojson-client"
 
 REQUIRES = [
     "geojson>=2.4.0",
@@ -10,15 +15,23 @@ REQUIRES = [
     "requests>=2.20.0",
 ]
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+VERSION = {}
+with open(os.path.join(HERE, NAME, "__version__.py")) as f:
+    exec(f.read(), VERSION)  # pylint: disable=exec-used
+
 setup(
-    name="geojson_client",
-    version="0.6",
-    author="Malte Franken",
-    author_email="coding@subspace.de",
-    description="A GeoJSON client library.",
+    name=NAME,
+    version=VERSION["__version__"],
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/exxamalte/python-geojson-client",
+    url=URL,
     packages=find_packages(exclude=("tests*",)),
     classifiers=[
         "Programming Language :: Python :: 3.7",
